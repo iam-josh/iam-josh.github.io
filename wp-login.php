e433d2576c326a14365d1c98326750da87efb3e5
Josh <136365328+iam-josh@users.noreply.github.com>
2023-06-14 21:24:46 +0100

    photosbyjosh files
    everything apart from wp-content

--- wp-login.php
+++ wp-login.php
@@ -4,17 +4,17 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Log In &lsaquo; Photos by Josh &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
-<link rel="stylesheet" id="dashicons-css" href="http://iam-josh.github.io/wp-includes/css/dashicons.min.css?ver=6.2.2" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="http://iam-josh.github.io/wp-includes/css/buttons.min.css?ver=6.2.2" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="http://iam-josh.github.io/wp-admin/css/forms.min.css?ver=6.2.2" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="http://iam-josh.github.io/wp-admin/css/l10n.min.css?ver=6.2.2" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="http://iam-josh.github.io/wp-admin/css/login.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://iam-josh.github.io/wp-includes/css/dashicons.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://iam-josh.github.io/wp-includes/css/buttons.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://iam-josh.github.io/wp-admin/css/forms.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://iam-josh.github.io/wp-admin/css/l10n.min.css?ver=6.2.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://iam-josh.github.io/wp-admin/css/login.min.css?ver=6.2.2" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-	<link rel="icon" href="http://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-32x32.webp" sizes="32x32">
-<link rel="icon" href="http://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-192x192.webp" sizes="192x192">
-<link rel="apple-touch-icon" href="http://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-180x180.webp">
-<meta name="msapplication-TileImage" content="http://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-270x270.webp">
+	<link rel="icon" href="https://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-32x32.webp" sizes="32x32">
+<link rel="icon" href="https://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-192x192.webp" sizes="192x192">
+<link rel="apple-touch-icon" href="https://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-180x180.webp">
+<meta name="msapplication-TileImage" content="https://iam-josh.github.io/wp-content/uploads/2021/09/cropped-Site-icon-270x270.webp">
 	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-gb">
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
@@ -45,12 +45,12 @@
 		</form>
 
 					<p id="nav">
-				<a href="http://iam-josh.github.io/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
+				<a href="https://iam-josh.github.io/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
 					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
-			<a href="http://iam-josh.github.io/">&larr; Go to Photos by Josh</a>		</p>
+			<a href="https://iam-josh.github.io/">&larr; Go to Photos by Josh</a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
@@ -70,19 +70,19 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 
 					</form>
 				</div>
-				<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
+				<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"http:\/\/iam-josh.github.io\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/iam-josh.github.io\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -96,14 +96,14 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2023-04-19 10:51:23+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/photosbyjosh\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
 var userProfileL10n = {"user_id":"0","nonce":"bb3fac4662"};
@@ -114,7 +114,7 @@ var userProfileL10n = {"user_id":"0","nonce":"bb3fac4662"};
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2023-04-19 10:51:23+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"Your new password has not been saved.":["Your new password has not been saved."],"Show":["Show"],"Hide":["Hide"],"Confirm use of weak password":["Confirm use of weak password"],"Hide password":["Hide password"],"Show password":["Show password"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
-<script type="text/javascript" src="http://iam-josh.github.io/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
+<script type="text/javascript" src="https://iam-josh.github.io/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
 	<script>/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);</script>
 		<div class="clear"></div>
 	</body>
@@ -126,6 +126,6 @@ Performance optimized by W3 Total Cache. Learn more: https://www.boldgrid.com/w3
 Page Caching using disk: enhanced (Requested URI contains query) 
 Minified using disk (Request URI is rejected)
 
-Served from: localhost @ 2023-06-14 13:57:47 by W3 Total Cache
+Served from: localhost @ 2023-06-14 20:52:04 by W3 Total Cache
 -->
 </html>
